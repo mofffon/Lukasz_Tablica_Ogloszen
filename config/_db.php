64b0afaf83770a9b97b55e8da03a3049b77ab4d2
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2025-07-13 21:12:07 +0200

    Add config folder

---
+++ config/_db.php
@@ -0,0 +1,10 @@
+<?php
+
+// Change name of this file to db.php and input your data  below
+return [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'ADD_DB_NAME',
+    'username' => 'ADD_DB_USER',
+    'password' => 'ADD_DB_PASSWORD'
+];
