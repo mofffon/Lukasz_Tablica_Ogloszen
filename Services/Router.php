4363483012dc8060679aaf978f04342384a1211f
Lukasz Jaskiewicz <lukasz.jaskiewicz2@gmail.com>
2025-07-13 20:33:37 +0200

    Add Services and in it Database, Router and Session

---
+++ Services/Router.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Services;
+
+class Router
+{
+
+    protected $routes = [];
+
+    /**
+     * Add a new route
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param string $action
+     * @param array $middleware
+     * 
+     * @return void
+     */
+    public function registerRoute($method, $uri, $action, $middleware = [])
+    {
+
+        list($controller, $controllerMethod) = explode('@', $action);
+
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+            'controllerMethod' => $controllerMethod,
+            'middleware' => $middleware
+        ];
+    }
+
+    /**
+     * Add a GET route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @param array $middleware
+     * @return void
+     */
+    public function get($uri, $controller, $middleware = [])
+    {
+        $this->registerRoute('GET', $uri, $controller, $middleware);
+    }
+
+    /**
+     * Add a POST route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @param array $middleware
+     * @return void
+     */
+    public function post($uri, $controller, $middleware = [])
+    {
+        $this->registerRoute('POST', $uri, $controller, $middleware);
+    }
+
+    /**
+     * Add a PUT route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @param array $middleware
+     * @return void
+     */
+    public function put($uri, $controller, $middleware = [])
+    {
+        $this->registerRoute('PUT', $uri, $controller, $middleware);
+    }
+
+    /**
+     * Add a DELETE route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @param array $middleware
+     * @return void
+     */
+    public function delete($uri, $controller, $middleware = [])
+    {
+        $this->registerRoute('DELETE', $uri, $controller, $middleware);
+    }
+
+
+    /**
+     * Route the request
+     * 
+     * @param string $uri
+     * @param string $method
+     * @return void
+     */
+    public function route($uri)
+    {
+
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+        // Check for _method input
+        if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+
+            // Override method input
+            $requestMethod = strtoupper($_POST['_method']);
+        }
+
+        foreach ($this->routes as $route) {
+
+            // Split the request uri into segments
+            $uriSegments = explode('/', trim($uri, '/'));
+
+            // Split the route uri into segments
+            $routeSegments = explode('/', trim($route['uri'], '/'));
+
+            $match = true;
+
+            // check if the number of segments matches
+            if (count($uriSegments) === count($routeSegments) && strtoupper($route['method']) === $requestMethod) {
+                $params = [];
+
+                for ($i = 0; $i < count($uriSegments); $i++) {
+                    // If the uri's do not match an there is no param
+                    if ($routeSegments[$i] !== $uriSegments[$i] && !preg_match('/\{(.+?)\}/', $routeSegments[$i])) {
+                        $match = false;
+                        break;
+                    }
+
+                    // Check for the params and add to the params array
+                    if (preg_match('/\{(.+?)\}/', $routeSegments[$i], $matches)) {
+                        $params[$matches[1]] = $uriSegments[$i];
+                    }
+                }
+
+                if ($match) {
+                    $controller = 'App\\controllers' . $route['controller'];
+                    $controllerMethod = $route['controllerMethod'];
+
+                    // Initiate the controller and call the method
+                    $controllerInstance = new $controller();
+                    $controllerInstance->$controllerMethod($params);
+                    return;
+                }
+            }
+        }
+
+        echo '404';
+    }
+}
